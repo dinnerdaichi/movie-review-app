7bb1d53d6fc994e1514e3feec93af535f441c658
dinnerdaichi <yamaguchi.daichandayo@gmail.com>
2024-11-18 16:19:49 +0900

    [update]orojin再追加

--- movie-review-app/src/server.ts
+++ movie-review-app/src/server.ts
@@ -4,7 +4,7 @@ import cors from "cors";
 import userRoutes from "./routes/user";
 import movieRoutes from "./routes/movie";
 
-const app:Express = express();
+const app: Express = express();
 const PORT = 3000;
 
 connectDB();
@@ -12,20 +12,19 @@ connectDB();
 // CORS設定を更新
 app.use(cors({
   origin: [
+    "https://movie-review-app-chi.vercel.app",
     "https://movie-review-l1uzt4i3i-dinnerdaichis-projects.vercel.app",
     "http://localhost:3000",
-    "http://localhost:5173"  // ローカル開発用のポート追加
+    "http://localhost:5173"
   ],
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true
 }));
 
-
 app.use(express.json());
 
-app.use('/api',userRoutes);
+app.use('/api', userRoutes);
 app.use('/api/movies', movieRoutes);
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
